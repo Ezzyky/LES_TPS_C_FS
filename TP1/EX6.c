0ad0c1d66dae099488a3c2bbc3b3cc9367f05ed1
Ezzyky <abdellahezzyky@gmail.com>
2026-04-24 17:16:42 +0100

    modified:   TP1/EX6.c

---
+++ TP1/EX6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14159265358979
+float surface(float r){
+    return PI*r*r;
+}
+float permitre(float r){
+    return 2*PI*r;
+}
+#include <stdio.h>
+int main (){
+
+    return 0;
+}
