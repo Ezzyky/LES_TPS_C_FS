de7637967382b8be9ff6cd9740c07dfae4e7e931
Ezzyky <abdellahezzyky@gmail.com>
2026-05-20 15:29:53 +0100

    Refactor EX4.c for improved input handling and clarity; update EX4.exe binary

--- TP1/EX4.c
+++ TP1/EX4.c
@@ -1,35 +1,39 @@
 #include <stdio.h>
 int main(){
-    int i,n,s=0,max=0,p,j;
-    do
+    int i,dimension,s=0,max=0,position,j;
+    printf("entre un valeur int: ");
+    scanf("%d",&dimension);
+    while (dimension>50 || dimension==0)
     {
-        printf("entre un valeur int (dimension maximale: 50composantes)");
-        scanf("%d",&n);
-    } while (n<=0 || n>50);
-    int t[n];
-   
-    p=0;
-    for(i=0;i<n;i++){
+        printf("(dimension maximale: 50 composantes)\n: ");
+        printf("entre un valeur int : ");
+        scanf("%d",&dimension);
+    }
+    int t[dimension];
+    position=0;
+    for(i=0;i<dimension;i++){
         printf ("entree la valeur d index %d:",i+1);
         scanf("%d",&t[i]);
         s+=t[i];
-    } 
-    max=t[0];
-    for(i=0;i<n;i++){
-       if (t[i]>max){
+        if (i==0) max=t[0];
+        if (t[i]>max){
             max=t[i];
-            p=i;
+            position=i;
         } 
-    for(i=0,j=0;i<n;i++){
-       if (t[i]!=0){
+        if (t[i]!=0){
             t[j]=t[i];
             j++;
-        } 
-        
+        }
+    } 
+    printf("\n\n-------------------------\n");
+    printf("le tableu sans la valeur 0:\n");
+    for (i=0;i<j;i++){
+        printf("----> %d\n",t[i]);
     }
-    printf("la somme de tableu :%d",s);
-    printf("la valeur max de ce tableu est :%d \n est ca position %d",max,p+1);
-}
+    printf("\n-------------------------\n");
+    printf("la somme de tableu: %d\n",s);
+    printf("\n-------------------------\n");
+    printf("la valeur max de ce tableu est :%d est ca position %d\n",max,position+1);
+    printf("\n-------------------------\n");
     return 0;
-
 }
