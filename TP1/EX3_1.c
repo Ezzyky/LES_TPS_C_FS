6c5301c558ecc67831b1d76fe26d3cf0e14815e8
Ezzyky <abdellahezzyky@gmail.com>
2026-05-20 12:06:25 +0100

    Refactor code structure for improved readability and maintainability

--- TP1/EX3_1.c
+++ TP1/EX3_1.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 int main(){
-    int n,p=0;
+    int n;
+    float p=0;
     printf("donner un entier:");
     scanf("%d",&n);
-   if (n>=30){
-       p=n-(n*(2/100));
+    if (n>=30){
+       p=n-(n*0.2);
+       printf("%2.f",p);
+    }
+    else {
+        printf("%d",n);
     }
     return 0;
 }
